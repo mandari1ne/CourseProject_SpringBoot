c041e8e8f0a8699ab91cb75b52682ddcb42c7cb0
mandari1ne <marina.stakhovets@gmail.com>
2025-04-01 17:37:47 +0300

    Initial commit

---
+++ src/main/java/org/example/kursach/KursachApplication.java
@@ -0,0 +1,13 @@
+package org.example.kursach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KursachApplication.class, args);
+    }
+
+}
