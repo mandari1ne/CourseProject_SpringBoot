f1ed4068c09e82a4d5bc2b71593bb7a1f8a32d02
mandari1ne <marina.stakhovets@gmail.com>
2025-04-01 22:18:59 +0300

    add profile info

--- src/main/java/org/example/kursach/controllers/AuthController.java
+++ src/main/java/org/example/kursach/controllers/AuthController.java
@@ -1,10 +1,15 @@
 package org.example.kursach.controllers;
 
 import org.example.kursach.model.User;
+import org.example.kursach.model.UserInfo;
 import org.example.kursach.services.UserService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/auth")
@@ -44,4 +49,22 @@ public class AuthController {
     public String homePage() {
         return "home";
     }
+
+    @GetMapping("/profile")
+    public String showAccountPage(Model model, Principal principal) {
+        String login = principal.getName();
+        User user = userService.findByLogin(login)
+                .orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден: " + login));
+
+        if (user.getUserInfo() == null) {
+            user.setUserInfo(new UserInfo());
+        }
+
+        model.addAttribute("user", user);
+        model.addAttribute("userInfo", user.getUserInfo());
+
+        return "profile";
+    }
+
+
 }
